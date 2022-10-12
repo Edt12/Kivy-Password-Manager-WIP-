2d946e9e55ad424e96e6b9553190315640953137
Edt12 <edtickle@icloud.com>
2022-10-12 22:03:26 +0100

    Added Keyboard broken -Fix tommorow

--- Kivytest.py
+++ Kivytest.py
@@ -28,7 +28,6 @@ cursor.execute("""create table IF NOT EXISTS UsersAndPasswords
 class Login(App):#Create different windows class
    
     def build(self):
-
  
         Window.clearcolor =(Grey)#sets background color for window to get value take each rgb value and divide by 255
 
@@ -41,7 +40,7 @@ class Login(App):#Create different windows class
         LoginLayout.add_widget(Password)
 
         EnterUsernameandPassword=Button(size_hint=(0.1,0.05),pos_hint={'x':0.7,'y':0.5},text="Enter",background_color=green)
-       
+        
    
         def Callback(self):
             cursor.execute("SELECT * From UsersAndPasswords")
@@ -71,7 +70,29 @@ class Login(App):#Create different windows class
         LoginLayout.add_widget(LoginTitle)
 
         return LoginLayout
-        
+    class KeyboardListener(Widget):
+        def __init__(self, **kwargs):
+            super(KeyboardListener, self).__init__(**kwargs)
+            self._keyboard = Window.request_keyboard(
+            self.k,self, 'tet')#requested a keyboard first function is what will be triggered when release is pressed 2nd is give keyboard attribute sef third is name of keyboard
+            if self._keyboard.widget:
+            # If it exists, this widget is a VKeyboard object which you can use
+            # to change the keyboard layout.
+                pass
+                self._keyboard.bind(on_key_down=self._on_keyboard_down)#Gives keydown variable
+  
+        def k(self):
+            print('My keyboard have been closed!')
+            self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+            self._keyboard = None
+
+        def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+            if keycode[40]=='enter':
+                keyboard.release() #calls first function in request command
+
+            # Return True to accept the key. Otherwise, it will be used by
+            # the system.
+            return True
 Login().run()
 cursor.execute("SELECT Username FROM UsersAndPasswords")#selects a table in database
 results=cursor.fetchall()#selects everything within that table
