d286eaab4380b2641543210094716eefb4d7f4cb
Edt12 <edtickle@icloud.com>
2022-10-20 20:32:15 +0100

    Added keyboard into app fix lat

--- Kivytest.py
+++ Kivytest.py
@@ -70,29 +70,7 @@ class Login(App):#Create different windows class
         LoginLayout.add_widget(LoginTitle)
 
         return LoginLayout
-    class KeyboardListener(Widget):
-        def __init__(self, **kwargs):
-            super(KeyboardListener, self).__init__(**kwargs)
-            self._keyboard = Window.request_keyboard(
-            self.k,self, 'tet')#requested a keyboard first function is what will be triggered when release is pressed 2nd is give keyboard attribute sef third is name of keyboard
-            if self._keyboard.widget:
-            # If it exists, this widget is a VKeyboard object which you can use
-            # to change the keyboard layout.
-                pass
-                self._keyboard.bind(on_key_down=self._on_keyboard_down)#Gives keydown variable
-  
-        def k(self):
-            print('My keyboard have been closed!')
-            self._keyboard.unbind(on_key_down=self._on_keyboard_down)
-            self._keyboard = None
-
-        def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-            if keycode[40]=='enter':
-                keyboard.release() #calls first function in request command
-
-            # Return True to accept the key. Otherwise, it will be used by
-            # the system.
-            return True
+         
 Login().run()
 cursor.execute("SELECT Username FROM UsersAndPasswords")#selects a table in database
 results=cursor.fetchall()#selects everything within that table
